26b704131b7539ba78d0fd138d442d89f1c24542
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-12-04 19:11:09 +0630

    added: form components

---
+++ src/components/form/RHFTextField.tsx
@@ -0,0 +1,70 @@
+// form
+import { Controller, useFormContext } from 'react-hook-form';
+// @mui
+import { Box, IconButton, TextField, TextFieldProps } from '@mui/material';
+
+import { useState } from 'react';
+import Iconify from '../iconify';
+
+// ----------------------------------------------------------------------
+
+type Props = TextFieldProps & {
+  name: string;
+  require?: boolean;
+  type?: string;
+};
+
+export default function RHFTextField({ name, require, type = 'text', ...other }: Props) {
+  const { control } = useFormContext();
+
+  const [inputType, setInputType] = useState(type);
+
+  const togglePassword = () => {
+    setInputType(inputType === 'password' ? 'text' : 'password');
+  };
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <Box sx={{ position: 'relative' }}>
+          <TextField
+            {...field}
+            fullWidth
+            type={inputType}
+            label={name}
+            sx={{ display: 'flex' }}
+            value={typeof field.value === 'number' && field.value === 0 ? '' : field.value || ''}
+            error={!!error}
+            helperText={error?.message}
+            autoComplete="off"
+            {...other}
+          />
+          {type === 'password' ? (
+            <Box onClick={togglePassword} sx={{ cursor: 'pointer' }}>
+              {inputType !== 'password' ? (
+                <Box sx={{ position: 'absolute', top: '12px', right: '1rem' }}>
+                  <IconButton edge="end">
+                    <Iconify
+                      icon={inputType !== 'password' ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                    />
+                  </IconButton>
+                </Box>
+              ) : (
+                <Box sx={{ position: 'absolute', top: '12px', right: '1rem' }}>
+                  <IconButton edge="end">
+                    <Iconify
+                      icon={inputType !== 'password' ? 'eva:eye-fill' : 'eva:eye-off-fill'}
+                    />
+                  </IconButton>
+                </Box>
+              )}
+            </Box>
+          ) : (
+            ''
+          )}
+        </Box>
+      )}
+    />
+  );
+}
