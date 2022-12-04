48614fb971f715f61a0486faa4b4978d8dc73621
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-12-04 19:10:25 +0630

    added: theme files

---
+++ src/theme/index.tsx
@@ -0,0 +1,48 @@
+import { useMemo } from "react";
+// @mui
+import { CssBaseline } from "@mui/material";
+import {
+	createTheme,
+	ThemeOptions,
+	StyledEngineProvider,
+	ThemeProvider as MUIThemeProvider,
+} from "@mui/material/styles";
+//
+import palette from "./palette";
+import typography from "./typography";
+import shadows from "./shadows";
+import customShadows from "./customShadows";
+import componentsOverride from "./overrides";
+import GlobalStyles from "./globalStyles";
+
+// ----------------------------------------------------------------------
+
+type Props = {
+	children: React.ReactNode;
+};
+
+export default function ThemeProvider({ children }: Props) {
+
+	const themeOptions: ThemeOptions = {
+		palette: palette("light"),
+		typography,
+		shape: { borderRadius: 8 },
+		direction: "rtl",
+		shadows: shadows("light"),
+		customShadows: customShadows("light"),
+	};
+
+	const theme = createTheme(themeOptions);
+
+	theme.components = componentsOverride(theme);
+
+	return (
+		<StyledEngineProvider injectFirst>
+			<MUIThemeProvider theme={theme}>
+				<CssBaseline />
+				<GlobalStyles />
+				{children}
+			</MUIThemeProvider>
+		</StyledEngineProvider>
+	);
+}
