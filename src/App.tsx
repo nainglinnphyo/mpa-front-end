c61d0d4c2d429d5ef04908f8220ac22b307b4cb1
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-11 20:21:29 +0630

    add snackbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import ThemeProvider from "./theme";
+import SnackbarProvider from "./components/snackbar";
 
 // custom components
 import store from "./store";
 import Routes from "./routes";
 
 const App = () => {
-	return (
-		<ThemeProvider>
-			<Provider store={store}>
-				<BrowserRouter>
-					<Routes />
-				</BrowserRouter>
-			</Provider>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider>
+      <Provider store={store}>
+        <SnackbarProvider>
+          <BrowserRouter>
+            <Routes />
+          </BrowserRouter>
+        </SnackbarProvider>
+      </Provider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
