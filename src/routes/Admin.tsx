88c6e4ea3d5a533cc7182cb7441092fa555285db
KyawZinThant22 <kyawthantver2@gmail.com>
2022-12-11 20:53:47 +0630

    route system modify

--- src/routes/Admin.tsx
+++ src/routes/Admin.tsx
@@ -5,23 +5,24 @@ import Dashboard from "../pages/admin/Dashboard";
 import Static from "../pages/admin/Static";
 
 const Admin = () => {
-	return useRoutes([
-		{ path: "/", element: <Navigate to="dashboard" replace /> },
-		{
-			path: "dashboard",
-			element: <Layout />,
-			children: [
-				{
-					path: "",
-					element: <Dashboard />,
-				},
-				{
-					path: "static",
-					element: <Static />,
-				},
-			],
-		},
-	]);
+  return useRoutes([
+    { path: "/", element: <Navigate to="dashboard" replace /> },
+    {
+      path: "dashboard",
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Dashboard />,
+        },
+        {
+          path: "static",
+          element: <Static />,
+        },
+      ],
+    },
+    { path: "*", element: <Navigate to="/dashboard" replace /> },
+  ]);
 };
 
 export default Admin;
