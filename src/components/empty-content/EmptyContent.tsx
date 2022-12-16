2806cb3db4a245322d51e620299eed23083b84cc
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2022-12-16 21:05:10 +0630

    required components

---
+++ src/components/empty-content/EmptyContent.tsx
@@ -0,0 +1,52 @@
+// @mui
+import { Typography, Stack, StackProps } from "@mui/material";
+//
+import Image from "../image";
+import FourOFour from "../../assets/fout0four.jpg";
+
+// ----------------------------------------------------------------------
+
+interface EmptyContentProps extends StackProps {
+	title: string;
+	img?: string;
+	description?: string;
+}
+
+export default function EmptyContent({
+	title,
+	description,
+	img,
+	sx,
+	...other
+}: EmptyContentProps) {
+	return (
+		<Stack
+			alignItems="center"
+			justifyContent="center"
+			sx={{
+				height: 1,
+				textAlign: "center",
+				p: (theme) => theme.spacing(8, 2),
+				...sx,
+			}}
+			{...other}
+		>
+			<Image
+				disabledEffect
+				alt="empty content"
+				src={img || FourOFour}
+				sx={{ height: 240, mb: 3 }}
+			/>
+
+			<Typography variant="h5" gutterBottom>
+				{title}
+			</Typography>
+
+			{description && (
+				<Typography variant="body2" sx={{ color: "text.secondary" }}>
+					{description}
+				</Typography>
+			)}
+		</Stack>
+	);
+}
