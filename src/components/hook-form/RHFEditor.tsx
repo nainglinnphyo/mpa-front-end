10b56b7dbb6693ac15b4d72722bc71218953deba
nainglinnphyo <nainglinnphyo24@gmail.com>
2023-01-01 12:30:09 +0630

    update ing

--- src/components/hook-form/RHFEditor.tsx
+++ src/components/hook-form/RHFEditor.tsx
@@ -1,37 +1,47 @@
-// form
-import { useFormContext, Controller } from 'react-hook-form';
-// @mui
-import { FormHelperText } from '@mui/material';
-//
-import Editor, { EditorProps } from '../editor';
+// // form
+// import { useFormContext, Controller } from 'react-hook-form';
+// // @mui
+// import { FormHelperText } from '@mui/material';
+// //
+// import Editor, { EditorProps } from '../editor';
 
-// ----------------------------------------------------------------------
+// // ----------------------------------------------------------------------
 
-interface Props extends EditorProps {
-  name: string;
-}
+// interface Props extends EditorProps {
+//   name: string;
+// }
 
-export default function RHFEditor({ name, ...other }: Props) {
-  const { control } = useFormContext();
+// export default function RHFEditor({ name, ...other }: Props) {
+//   const { control } = useFormContext();
 
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field, fieldState: { error } }) => (
+//         <Editor
+//           id={name}
+//           value={field.value}
+//           onChange={field.onChange}
+//           error={!!error}
+//           helperText={
+//             <FormHelperText error sx={{ px: 2, textTransform: 'capitalize' }}>
+//               {error?.message}
+//             </FormHelperText>
+//           }
+//           {...other}
+//         />
+//       )}
+//     />
+//   );
+// }
+
+import React from 'react'
+
+function RHFEditor() {
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState: { error } }) => (
-        <Editor
-          id={name}
-          value={field.value}
-          onChange={field.onChange}
-          error={!!error}
-          helperText={
-            <FormHelperText error sx={{ px: 2, textTransform: 'capitalize' }}>
-              {error?.message}
-            </FormHelperText>
-          }
-          {...other}
-        />
-      )}
-    />
-  );
+    <div>RHFEditor</div>
+  )
 }
+
+export default RHFEditor
