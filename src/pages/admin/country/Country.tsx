8e6338539691c83ab84146bfa6d195e87c9cfe2e
KyawZinThant22 <kyawthantver2@gmail.com>
2023-01-11 22:35:35 +0630

    country list

--- src/pages/admin/country/Country.tsx
+++ src/pages/admin/country/Country.tsx
@@ -1,315 +1,268 @@
 import { useState, useEffect } from "react";
 // @mui
 import {
-	Card,
-	Table,
-	Button,
-	Tooltip,
-	TableBody,
-	Container,
-	IconButton,
-	TableContainer,
+  Card,
+  Table,
+  Button,
+  Tooltip,
+  TableBody,
+  Container,
+  IconButton,
+  TableContainer,
 } from "@mui/material";
 // // components
 import Iconify from "../../../components/iconify/Iconify";
 import {
-	useTable,
-	getComparator,
-	emptyRows,
-	TableNoData,
-	TableEmptyRows,
-	TableHeadCustom,
-	TableSelectedAction,
-	TablePaginationCustom,
+  useTable,
+  getComparator,
+  emptyRows,
+  TableNoData,
+  TableEmptyRows,
+  TableHeadCustom,
+  TableSelectedAction,
+  TablePaginationCustom,
 } from "../../../components/table";
 // sections
 import { CountryTableToolbar, CountryTableRow } from "./components";
+import { getCountry } from "../../../store/reducers/country";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { RootState } from "../../../store";
 
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-	{ id: "countryOrigin", label: "Country Origin", align: "center" },
-	{ id: "countryReturn", label: "Country Return", align: "center" },
-	{ id: "createdBy", label: "Created By", align: "center" },
-	{ id: "createdAt", label: "Created At", align: "center" },
-	{ id: "action", label: "Action", align: "right" },
+  { id: "name", label: "Name", align: "center" },
+  { id: "createdAt", label: "Created At", align: "center" },
+  { id: "action", label: "Action" },
 ];
 
 // ----------------------------------------------------------------------
 
 export interface CountryList {
-	id: string;
-	countryOrigin: string;
-	countryReturn: string;
-	createdBy: string;
-	createdAt: string;
+  id: string;
+  name: string;
+  created_at: string;
 }
 
 export default function CountryPage() {
-	const {
-		dense,
-		page,
-		order,
-		orderBy,
-		rowsPerPage,
-		setPage,
-		//
-		selected,
-		setSelected,
-		onSelectRow,
-		onSelectAllRows,
-		//
-		onSort,
-		onChangeDense,
-		onChangePage,
-		onChangeRowsPerPage,
-	} = useTable();
+  const {
+    dense,
+    page,
+    order,
+    orderBy,
+    rowsPerPage,
+    setPage,
+    //
+    selected,
+    setSelected,
+    onSelectRow,
+    onSelectAllRows,
+    //
+    onSort,
+    onChangeDense,
+    onChangePage,
+    onChangeRowsPerPage,
+  } = useTable();
 
-	const [tableData, setTableData] = useState<CountryList[]>([]);
+  const [tableData, setTableData] = useState<CountryList[]>([]);
 
-	const [filterName, setFilterName] = useState("");
+  const [filterName, setFilterName] = useState("");
 
-	const [filterRole, setFilterRole] = useState("all");
+  const [filterRole, setFilterRole] = useState("all");
 
-	const [filterStatus, setFilterStatus] = useState("all");
+  const [filterStatus, setFilterStatus] = useState("all");
 
-	const dataFiltered = applyFilter({
-		inputData: tableData,
-		comparator: getComparator(order, orderBy),
-		filterName,
-		filterRole,
-		filterStatus,
-	});
+  const dataFiltered = applyFilter({
+    inputData: tableData,
+    comparator: getComparator(order, orderBy),
+    filterName,
+    filterRole,
+    filterStatus,
+  });
 
-	const dataInPage = dataFiltered.slice(
-		page * rowsPerPage,
-		page * rowsPerPage + rowsPerPage
-	);
+  const dataInPage = dataFiltered.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
 
-	const denseHeight = dense ? 52 : 72;
+  const denseHeight = dense ? 52 : 72;
 
-	const isFiltered =
-		filterName !== "" || filterRole !== "all" || filterStatus !== "all";
+  const isFiltered =
+    filterName !== "" || filterRole !== "all" || filterStatus !== "all";
 
-	const isNotFound =
-		(!dataFiltered.length && !!filterName) ||
-		(!dataFiltered.length && !!filterRole) ||
-		(!dataFiltered.length && !!filterStatus);
+  const isNotFound =
+    (!dataFiltered.length && !!filterName) ||
+    (!dataFiltered.length && !!filterRole) ||
+    (!dataFiltered.length && !!filterStatus);
 
-	const handleFilterName = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setPage(0);
-		setFilterName(event.target.value);
-	};
+  const handleFilterName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPage(0);
+    setFilterName(event.target.value);
+  };
 
-	const handleDeleteRow = (id: string) => {
-		const deleteRow = tableData.filter((row: any) => row.id !== id);
-		setSelected([]);
-		setTableData(deleteRow);
+  const handleDeleteRow = (id: string) => {
+    const deleteRow = tableData.filter((row: any) => row.id !== id);
+    setSelected([]);
+    setTableData(deleteRow);
 
-		if (page > 0) {
-			if (dataInPage.length < 2) {
-				setPage(page - 1);
-			}
-		}
-	};
+    if (page > 0) {
+      if (dataInPage.length < 2) {
+        setPage(page - 1);
+      }
+    }
+  };
 
-	const handleEditRow = (id: string) => {
-		alert("edit");
-	};
+  const handleEditRow = (id: string) => {
+    alert("edit");
+  };
 
-	const handleResetFilter = () => {
-		setFilterName("");
-		setFilterRole("all");
-		setFilterStatus("all");
-	};
+  const handleResetFilter = () => {
+    setFilterName("");
+    setFilterRole("all");
+    setFilterStatus("all");
+  };
 
-	useEffect(() => {
-		setTableData([
-			{
-				id: "1",
-				countryOrigin: "Myanmar",
-				countryReturn: "China",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "2",
-				countryOrigin: "Myanmar",
-				countryReturn: "India",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "3",
-				countryOrigin: "Myanmar",
-				countryReturn: "Spain",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "4",
-				countryOrigin: "Myanmar",
-				countryReturn: "Korea",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-      {
-				id: "5",
-				countryOrigin: "Myanmar",
-				countryReturn: "China",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "6",
-				countryOrigin: "Myanmar",
-				countryReturn: "India",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "7",
-				countryOrigin: "Myanmar",
-				countryReturn: "Spain",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-			{
-				id: "8",
-				countryOrigin: "Myanmar",
-				countryReturn: "Korea",
-				createdAt: new Date().toISOString(),
-				createdBy: "Admin",
-			},
-		]);
-	}, []);
+  const dispatch = useAppDispatch();
+  const { data, error, isLoading } = useAppSelector(
+    (state: RootState) => state.country
+  );
 
-	return (
-		<Container maxWidth="xl">
-			<Card
-				sx={{
-					width: "100%",
-					height: "100%",
-					overflowY: "scroll",
-					mt: 2,
-				}}
-			>
-				<CountryTableToolbar
-					isFiltered={isFiltered}
-					filterName={filterName}
-					onFilterName={handleFilterName}
-					onResetFilter={handleResetFilter}
-				/>
+  const { token } = useAppSelector((state: RootState) => state.auth);
 
-				<TableContainer sx={{ position: "relative", overflow: "unset" }}>
-					<TableSelectedAction
-						dense={dense}
-						numSelected={selected.length}
-						rowCount={tableData.length}
-						onSelectAllRows={(checked) =>
-							onSelectAllRows(
-								checked,
-								tableData.map((row) => row.id)
-							)
-						}
-						action={
-							<Tooltip title="Delete">
-								<IconButton color="primary">
-									<Iconify icon="eva:trash-2-outline" />
-								</IconButton>
-							</Tooltip>
-						}
-					/>
+  useEffect(() => {
+    dispatch(getCountry(token));
+  }, [token, dispatch]);
 
-					{/* <Scrollbar> */}
-					<Table size={dense ? "small" : "medium"} sx={{ minWidth: 800 }}>
-						<TableHeadCustom
-							order={order}
-							orderBy={orderBy}
-							headLabel={TABLE_HEAD}
-							rowCount={tableData.length}
-							numSelected={selected.length}
-							onSort={onSort}
-							onSelectAllRows={(checked) =>
-								onSelectAllRows(
-									checked,
-									tableData.map((row) => row.id)
-								)
-							}
-						/>
+  useEffect(() => {
+    if (data.length) {
+      setTableData(data);
+    }
+  }, [data]);
 
-						<TableBody>
-							{dataFiltered
-								.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-								.map((row) => (
-									<CountryTableRow
-										key={row.id}
-										row={row}
-										selected={selected.includes(row.id)}
-										onSelectRow={() => onSelectRow(row.id)}
-										onDeleteRow={() => handleDeleteRow(row.id)}
-										onEditRow={() => handleEditRow(row.countryOrigin)}
-									/>
-								))}
+  return (
+    <Container maxWidth="xl">
+      <Card
+        sx={{
+          width: "100%",
+          height: "100%",
+          overflowY: "scroll",
+          mt: 2,
+        }}
+      >
+        <CountryTableToolbar
+          isFiltered={isFiltered}
+          filterName={filterName}
+          onFilterName={handleFilterName}
+          onResetFilter={handleResetFilter}
+        />
 
-							<TableEmptyRows
-								height={denseHeight}
-								emptyRows={emptyRows(page, rowsPerPage, tableData.length)}
-							/>
+        <TableContainer sx={{ position: "relative", overflow: "unset" }}>
+          <TableSelectedAction
+            dense={dense}
+            numSelected={selected.length}
+            rowCount={tableData.length}
+            onSelectAllRows={(checked) =>
+              onSelectAllRows(
+                checked,
+                tableData.map((row) => row.id)
+              )
+            }
+            action={
+              <Tooltip title="Delete">
+                <IconButton color="primary">
+                  <Iconify icon="eva:trash-2-outline" />
+                </IconButton>
+              </Tooltip>
+            }
+          />
 
-							<TableNoData isNotFound={isNotFound} />
-						</TableBody>
-					</Table>
-					{/* </Scrollbar> */}
-				</TableContainer>
+          {/* <Scrollbar> */}
+          <Table size={dense ? "small" : "medium"} sx={{ minWidth: 800 }}>
+            <TableHeadCustom
+              order={order}
+              orderBy={orderBy}
+              headLabel={TABLE_HEAD}
+              rowCount={tableData.length}
+              numSelected={selected.length}
+              onSort={onSort}
+              onSelectAllRows={(checked) =>
+                onSelectAllRows(
+                  checked,
+                  tableData.map((row) => row.id)
+                )
+              }
+            />
 
-				<TablePaginationCustom
-					count={dataFiltered.length}
-					page={page}
-					rowsPerPage={rowsPerPage}
-					onPageChange={onChangePage}
-					onRowsPerPageChange={onChangeRowsPerPage}
-					//
-					dense={dense}
-					onChangeDense={onChangeDense}
-				/>
-			</Card>
-		</Container>
-	);
+            <TableBody>
+              {dataFiltered
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => (
+                  <CountryTableRow
+                    key={row.id}
+                    row={row}
+                    selected={selected.includes(row.id)}
+                    onSelectRow={() => onSelectRow(row.id)}
+                    onDeleteRow={() => handleDeleteRow(row.id)}
+                    onEditRow={() => handleEditRow(row.name)}
+                  />
+                ))}
+
+              <TableEmptyRows
+                height={denseHeight}
+                emptyRows={emptyRows(page, rowsPerPage, tableData.length)}
+              />
+
+              <TableNoData isNotFound={isNotFound} />
+            </TableBody>
+          </Table>
+          {/* </Scrollbar> */}
+        </TableContainer>
+
+        <TablePaginationCustom
+          count={dataFiltered.length}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          onPageChange={onChangePage}
+          onRowsPerPageChange={onChangeRowsPerPage}
+          //
+          dense={dense}
+          onChangeDense={onChangeDense}
+        />
+      </Card>
+    </Container>
+  );
 }
 
 // ----------------------------------------------------------------------
 
 function applyFilter({
-	inputData,
-	comparator,
-	filterName,
-	filterStatus,
-	filterRole,
+  inputData,
+  comparator,
+  filterName,
+  filterStatus,
+  filterRole,
 }: {
-	inputData: CountryList[];
-	comparator: (a: any, b: any) => number;
-	filterName: string;
-	filterStatus: string;
-	filterRole: string;
+  inputData: CountryList[];
+  comparator: (a: any, b: any) => number;
+  filterName: string;
+  filterStatus: string;
+  filterRole: string;
 }) {
-	const stabilizedThis = inputData.map((el, index) => [el, index] as const);
+  const stabilizedThis = inputData.map((el, index) => [el, index] as const);
 
-	stabilizedThis.sort((a, b) => {
-		const order = comparator(a[0], b[0]);
-		if (order !== 0) return order;
-		return a[1] - b[1];
-	});
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
 
-	inputData = stabilizedThis.map((el) => el[0]);
+  inputData = stabilizedThis.map((el) => el[0]);
 
-	if (filterName) {
-		inputData = inputData.filter(
-			(user) =>
-				user.countryOrigin.toLowerCase().indexOf(filterName.toLowerCase()) !==
-				-1
-		);
-	}
+  if (filterName) {
+    inputData = inputData.filter(
+      (user) => user.name.toLowerCase().indexOf(filterName.toLowerCase()) !== -1
+    );
+  }
 
-	return inputData;
+  return inputData;
 }
--- src/pages/admin/country/components/CountryTableRow.tsx
+++ src/pages/admin/country/components/CountryTableRow.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 // @mui
 import {
-	Checkbox,
-	TableRow,
-	MenuItem,
-	TableCell,
-	IconButton,
+  Checkbox,
+  TableRow,
+  MenuItem,
+  TableCell,
+  IconButton,
 } from "@mui/material";
 import Iconify from "../../../../components/iconify";
 import MenuPopover from "../../../../components/menu-popover";
@@ -15,98 +15,90 @@ import moment from "moment";
 // ----------------------------------------------------------------------
 
 type Props = {
-	row: CountryList;
-	selected: boolean;
-	onEditRow: VoidFunction;
-	onSelectRow: VoidFunction;
-	onDeleteRow: VoidFunction;
+  row: CountryList;
+  selected: boolean;
+  onEditRow: VoidFunction;
+  onSelectRow: VoidFunction;
+  onDeleteRow: VoidFunction;
 };
 
 export default function UserTableRow({
-	row,
-	selected,
-	onEditRow,
-	onSelectRow,
-	onDeleteRow,
+  row,
+  selected,
+  onEditRow,
+  onSelectRow,
+  onDeleteRow,
 }: Props) {
-	const { countryOrigin, countryReturn, id, createdAt, createdBy } = row;
+  const { created_at, id, name } = row;
 
-	const [openConfirm, setOpenConfirm] = useState(false);
+  const [openConfirm, setOpenConfirm] = useState(false);
 
-	const [openPopover, setOpenPopover] = useState<HTMLElement | null>(null);
+  const [openPopover, setOpenPopover] = useState<HTMLElement | null>(null);
 
-	const handleOpenConfirm = () => {
-		setOpenConfirm(true);
-	};
+  const handleOpenConfirm = () => {
+    setOpenConfirm(true);
+  };
 
-	const handleCloseConfirm = () => {
-		setOpenConfirm(false);
-	};
+  const handleCloseConfirm = () => {
+    setOpenConfirm(false);
+  };
 
-	const handleOpenPopover = (event: React.MouseEvent<HTMLElement>) => {
-		setOpenPopover(event.currentTarget);
-	};
+  const handleOpenPopover = (event: React.MouseEvent<HTMLElement>) => {
+    setOpenPopover(event.currentTarget);
+  };
 
-	const handleClosePopover = () => {
-		setOpenPopover(null);
-	};
+  const handleClosePopover = () => {
+    setOpenPopover(null);
+  };
 
-	return (
-		<>
-			<TableRow hover selected={selected}>
-				<TableCell padding="checkbox">
-					<Checkbox checked={selected} onClick={onSelectRow} />
-				</TableCell>
+  return (
+    <>
+      <TableRow hover selected={selected}>
+        <TableCell padding="checkbox">
+          <Checkbox checked={selected} onClick={onSelectRow} />
+        </TableCell>
 
-				<TableCell align="center">{countryOrigin}</TableCell>
+        <TableCell align="center">{name}</TableCell>
 
-				<TableCell align="center">{countryReturn}</TableCell>
+        <TableCell align="center">{created_at}</TableCell>
 
-				<TableCell align="center" sx={{ textTransform: "capitalize" }}>
-					{createdBy}
-				</TableCell>
+        <TableCell align="right">
+          <IconButton
+            color={openPopover ? "inherit" : "default"}
+            onClick={handleOpenPopover}
+          >
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
 
-				<TableCell align="center" sx={{ textTransform: "capitalize" }}>
-					{moment(createdAt || moment()).format("DD/MM/YYYY hh:mm:ss A")}
-				</TableCell>
+      <MenuPopover
+        open={openPopover}
+        onClose={handleClosePopover}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            handleOpenConfirm();
+            handleClosePopover();
+          }}
+          sx={{ color: "error.main" }}
+        >
+          <Iconify icon="eva:trash-2-outline" />
+          Delete
+        </MenuItem>
 
-				<TableCell align="right">
-					<IconButton
-						color={openPopover ? "inherit" : "default"}
-						onClick={handleOpenPopover}
-					>
-						<Iconify icon="eva:more-vertical-fill" />
-					</IconButton>
-				</TableCell>
-			</TableRow>
-
-			<MenuPopover
-				open={openPopover}
-				onClose={handleClosePopover}
-				arrow="right-top"
-				sx={{ width: 140 }}
-			>
-				<MenuItem
-					onClick={() => {
-						handleOpenConfirm();
-						handleClosePopover();
-					}}
-					sx={{ color: "error.main" }}
-				>
-					<Iconify icon="eva:trash-2-outline" />
-					Delete
-				</MenuItem>
-
-				<MenuItem
-					onClick={() => {
-						onEditRow();
-						handleClosePopover();
-					}}
-				>
-					<Iconify icon="eva:edit-fill" />
-					Edit
-				</MenuItem>
-			</MenuPopover>
-		</>
-	);
+        <MenuItem
+          onClick={() => {
+            onEditRow();
+            handleClosePopover();
+          }}
+        >
+          <Iconify icon="eva:edit-fill" />
+          Edit
+        </MenuItem>
+      </MenuPopover>
+    </>
+  );
 }
